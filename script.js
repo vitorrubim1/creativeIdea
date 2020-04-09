2a4d55daf631aa64c709638dbbbee69942d55ffe
Vitor Rubim <vitorrubim1@hotmail.com>
2020-04-08 22:12:56 -0300

    feat: Criado a página de ideias, estruturando o HTML e CSS e dando interatividade com JS

--- script.js
+++ script.js
@@ -3,4 +3,15 @@ function onOff(){
         .querySelector("#modal")
         .classList
         .toggle("hide")
+
+    //TIRANDO A SCROOLANGEM CASO O MODAL ESTIVER ABERTO
+    document 
+        .querySelector("body")
+        .classList
+        .toggle("hideScrool")
+
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("addScrool")
 }
